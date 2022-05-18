0992614bd4895fddab08407d3a5aa0a655bf29a8
Shawlycode <kutinaorlando@gmail.com>
2022-05-18 15:00:02 +0000

    code debuged

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <link>
+        https://gist.github.com/albertfiati/0a0a747b8f562796a4d140942a9e1712?permalink_comment_id=4170997#gistcomment-4170997
+      </link>
     </div>
   );
 }
---
+++ src/Debug.js
@@ -0,0 +1,7 @@
+
+
+
+https://gist.github.com/albertfiati/0a0a747b8f562796a4d140942a9e1712?permalink_comment_id=4170997#gistcomment-4170997
+
+
+// kindly find above the debug link for   queston 4.
